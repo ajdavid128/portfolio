7ff5b4648810e89438aacb357b232ef1b9a58415
Aaron David <ajdavid128@gmail.com>
2023-03-29 17:16:47 -0500

    confirming previous git commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import About from "./Components/About";
+import Projects from "./Components/Projects";
 
 function App() {
   return (
     <div >
-      <h1>Hello World!</h1>
+      <h1>Hi, welcome to my portfolio site! I'm Bob Ross.</h1>
     </div>
   );
 }
