73e3d569477f55141ff28b2a4571dcca11bfcd55
Aaron David <ajdavid128@gmail.com>
2023-05-01 15:58:22 -0500

    broke up nav links, added text to landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -3,7 +3,11 @@
 
 function Landing() {
     return (
+        <>
         <h1>Landing</h1>
+        <h3>Hey, my name is Aaron David and welcome ot my site!</h3>
+        
+        </>
     )
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,8 +7,11 @@ function Nav() {
         <>
             <h1>Navvvvvv</h1>
             <Link to="/about">About</Link>
+            <br/>
             <Link to="/contact">Contact</Link>
+            <br/>
             <Link to="/experience">Experience</Link>
+            <br/>
             <Link to="/projects">Projects</Link>
             <Outlet />
         </>
