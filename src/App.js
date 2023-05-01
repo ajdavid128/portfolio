8eecac61df4aae6fdb7a0e68c180fac167265852
Aaron David <ajdavid128@gmail.com>
2023-05-01 15:40:34 -0500

    added browserRouter components to App.js, added Nav and NoPage components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
 import Landing from "./Components/Landing";
+import Experience from "./Components/Experience";
+import Contact from "./Components/Contact";
+import NoPage from "./Components/NoPage";
+import Nav from "./Components/Nav";
+
 
 function App() {
   return (
-    <div >
-      <h1>Hi, welcome to my portfolio site! I'm Bob Ross.</h1>
-    </div>
+    // <div >
+    //   <h1>Hi, welcome to my portfolio site! I'm Bob Ross.</h1>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Landing />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,9 @@
+
+
+function Nav() {
+    return (
+        <h1>Navvvvvv</h1>
+    )
+}
+
+export default Nav;
