a596544bbbef771c3f4e78d473d0c0986b2a8caf
Aaron David <ajdavid128@gmail.com>
2023-05-01 15:50:15 -0500

    added Links and Outlet to Nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,17 @@
+import { Outlet, Link } from "react-router-dom";
+
 
 
 function Nav() {
     return (
-        <h1>Navvvvvv</h1>
+        <>
+            <h1>Navvvvvv</h1>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/experience">Experience</Link>
+            <Link to="/projects">Projects</Link>
+            <Outlet />
+        </>
     )
 }
 
