15d9222bf83f7edb312625f54cb43b4af42b7fd3
Aaron David <ajdavid128@gmail.com>
2023-05-02 11:23:26 -0500

    added meida folder and image of myself, added img element and image of me to landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,12 +1,17 @@
+import me1 from "../Media/me1.png";
 
 
 
 function Landing() {
     return (
         <>
-        <h1>Landing</h1>
-        <h3>Hey, my name is Aaron David and welcome ot my site!</h3>
-        
+            <h1>Landing</h1>
+            <div className="centered">
+                <img src={me1} alt="Aaron David portrait"/>
+            </div>
+            <div className="centered">
+                <h3>Hey, my name is Aaron David and welcome to my site!</h3>
+            </div>
         </>
     )
 }
