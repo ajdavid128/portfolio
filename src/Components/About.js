b346e44e11a7392209951129fbdf23db8b5d2c4b
Aaron David <ajdavid128@gmail.com>
2023-05-03 15:32:31 -0500

    added photo and sample text to about section, added state and toggle capabilities to switch between blurb about me and embedded video

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,33 @@
+import { useState } from "react";
 
 import EmbedVideo from "./EmbedVideo";
+import BWme from "../Media/BWme.jpg";
 
 
 function About() {
+
+    const [toggleVideo, setToggleVideo] = useState(false)
+
+    const showVideo = () => {
+        setToggleVideo(!toggleVideo);
+    }
+
     return (
         <div id="about-page-container">
             <h1>About: The History of Cool Beans</h1>
+            {toggleVideo ? 
             <div id="video-container">
                 <EmbedVideo embedId="PyHPSrk18d4" />
+                <button onClick={showVideo}>click me</button>
             </div>
+            
+            :
+            <div id="about-text-photo">
+                <img id="about-photo" src={BWme} alt="Aaron David portrait"/>
+                <p> I am a Full-Stack Software Engineer with programming experience centered around React.js and Ruby on Rails based web-development. My bachelor's degree is in Fine Art and I have professional experience in textile manufacturing. I am well versed in team leadership, production and project management, process optimization, organization and delegation. As a highly motivated, multi-tasking individual that thrives when confronted with a problem in need of a solution I am quick to learn, can appreciate a challenge and I excel in a hands-on work environment that encourages creativity. I pride myself on my patience, ability to communicate clearly and concisely, and strive to bring teams together to achieve mutual goals.</p>
+                <button onClick={showVideo} >click me</button>
+            </div>
+            }
         </div>
     )
 };
