e9033f455a988d53e1f27665a3c30dc2f83856ce
Aaron David <ajdavid128@gmail.com>
2023-05-03 14:41:06 -0500

    created EmbedVideo component and added elevator pitch youtube video

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,12 @@
 
+import EmbedVideo from "./EmbedVideo";
+
 
 function About() {
     return (
         <div>
             <h1>About: The History of Cool Beans</h1>
-            
+            <EmbedVideo embedId="PyHPSrk18d4" />
         </div>
     )
 };
