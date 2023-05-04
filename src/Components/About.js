b15f5111af3f9d18de4df4f2973bef4a066c5a65
Aaron David <ajdavid128@gmail.com>
2023-05-04 17:09:51 -0500

    adjusting about page grid styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,11 +23,15 @@ function About() {
             
             :
             <div id="about-text-photo">
-                <img id="about-photo" src={BWme} alt="Aaron David portrait"/>
-                <section>
+                <div>
+                    <img id="about-photo" src={BWme} alt="Aaron David portrait"/>
+                </div>
+                <div id="about-text">
                     <p> I am a Full-Stack Software Engineer with programming experience centered around React.js and Ruby on Rails based web-development. My bachelor's degree is in Fine Art and I have professional experience in textile manufacturing. I am well versed in team leadership, production and project management, process optimization, organization and delegation. As a highly motivated, multi-tasking individual that thrives when confronted with a problem in need of a solution I am quick to learn, can appreciate a challenge and I excel in a hands-on work environment that encourages creativity. I pride myself on my patience, ability to communicate clearly and concisely, and strive to bring teams together to achieve mutual goals.</p>
-                </section>
-                <button onClick={showVideo} >click me</button>
+                </div>
+                <div>
+                    <button onClick={showVideo} id="box3">click me</button>
+                </div>
             </div>
             }
         </div>
