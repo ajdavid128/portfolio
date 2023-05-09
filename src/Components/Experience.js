4f22f0dd22fa66ba71e7d73ca5607fafb70a7766
Aaron David <ajdavid128@gmail.com>
2023-05-09 16:43:20 -0500

    added nav arrows to experience page

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,8 +1,20 @@
+import { Icon } from "semantic-ui-react";
 
 
 function Experience() {
     return (
-        <h1>Experience</h1>
+        <>
+            <h1>Experience</h1>
+            <div className="nav-left-arrow">
+                <Icon link  name="caret left" size="big"/>
+            </div>
+            <div className="nav-down-arrow">
+                <Icon link  name="caret down" size="big"/>
+            </div>
+            <div className="nav-right-arrow">
+                <Icon link  name="caret right" size="big"/>
+            </div>
+        </>
     )
 }
 
