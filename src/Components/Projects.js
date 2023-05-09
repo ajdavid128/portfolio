84ef385eab3551f2a97425d13cc37afd76cc3ecc
Aaron David <ajdavid128@gmail.com>
2023-05-09 16:44:29 -0500

    added nav arrows to projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,4 @@
-
+import { Icon } from "semantic-ui-react";
 import Project1 from "./Project1";
 import Project2 from "./Project2";
 
@@ -9,6 +9,15 @@ function Projects() {
             <h1>Projects</h1>
             <Project1 />
             <Project2 />
+            <div className="nav-left-arrow">
+                <Icon link  name="caret left" size="big"/>
+            </div>
+            <div className="nav-down-arrow">
+                <Icon link  name="caret down" size="big"/>
+            </div>
+            <div className="nav-right-arrow">
+                <Icon link  name="caret right" size="big"/>
+            </div>
         </div>
     )
 }
