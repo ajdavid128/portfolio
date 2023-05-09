ba0aaf52d1a5e6312a47a21dbb024c2f66915471
Aaron David <ajdavid128@gmail.com>
2023-05-09 16:42:10 -0500

    added nav arrows to contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,20 @@
+import { Icon } from "semantic-ui-react";
 
 
 function Contact() {
     return (
-        <h1>Contact</h1>
+        <>
+            <h1>Contact</h1>
+            <div className="nav-left-arrow">
+                <Icon link  name="caret left" size="big"/>
+            </div>
+            <div className="nav-down-arrow">
+                <Icon link  name="caret down" size="big"/>
+            </div>
+            <div className="nav-right-arrow">
+                <Icon link  name="caret right" size="big"/>
+            </div>
+        </>
     )
 }
 
