308e6dd3091ae2babda6b60333337a57075865c1
Aaron David <ajdavid128@gmail.com>
2023-05-11 17:32:04 -0500

    deleted unused NavArrow component and commented out Nav route in app component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Experience from "./Components/Experience";
 import Contact from "./Components/Contact";
 import NoPage from "./Components/NoPage";
 import Nav from "./Components/Nav";
-import NavArrows from "./Components/NavArrows";
+
 
 
 function App() {
@@ -17,14 +17,14 @@ function App() {
     // </div>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Nav />}>
+        {/* <Route path="/" element={<Nav />}> */}
           <Route index element={<Landing />} />
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
           <Route path="experience" element={<Experience />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
-        </Route>
+        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
